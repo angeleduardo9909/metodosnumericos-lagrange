f5c19258d24fe3122d46d61b0a0ba424e83a3736
angeleduardo9909 <61066496+angeleduardo9909@users.noreply.github.com>
2020-05-18 17:17:12 -0500

    Add files via upload

---
+++ src/metodolagrange/MetodoLagrange.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodolagrange;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MetodoLagrange {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmVentana v = new frmVentana();
+        v.setVisible(true);
+    }
+    
+}
